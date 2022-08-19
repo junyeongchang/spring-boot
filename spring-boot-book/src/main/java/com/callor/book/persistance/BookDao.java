d174a882cb7479f61d6c598f43d922c5317b3a5d
junyeongchang <jychang11@naver.com>
2022-08-19 09:16:18 +0900

    20220819

---
+++ spring-boot-book/src/main/java/com/callor/book/persistance/BookDao.java
@@ -0,0 +1,8 @@
+package com.callor.book.persistance;
+
+import com.callor.book.model.BookVO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookDao extends JpaRepository<BookVO, String> {
+
+}
---
+++ spring-boot-book/src/main/java/com/callor/book/service/BookService.java
@@ -0,0 +1,17 @@
+package com.callor.book.service;
+
+import com.callor.book.model.BookVO;
+
+import java.util.List;
+
+public interface BookService {
+    public List<BookVO> selectAll();
+    public BookVO findById(String isbm);
+    public int insert(BookVO bookVO);
+    public int update(BookVO bookVO);
+    public int delete(BookVO bookVO);
+
+    public List<BookVO> findByTitle(String title);
+    public List<BookVO> findByComp(String comp);
+    public List<BookVO> findByAuthor(String author);
+}
